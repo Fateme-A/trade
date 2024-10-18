89f22d4b1f502a1bf187a35c395e0298a08575a3
Fateme-A <askarifatemeh96@gmail.com>
2024-10-19 00:18:24 +0330

    feat(tailwind): add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4ef09d",
+        neutral: {
+          darker: "#1a1a1a",
+          dark: "#333333",
+          normal: "#B2B2B2",
+          light: "#F2F2F2",
+          white: "#FFFFFF",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+  darkMode: "class",
+};

148c8529d7bc7a178af8c6fc3760569ef42c63d7
Fateme-A <askarifatemeh96@gmail.com>
2024-10-19 00:23:10 +0330

    feat: using provider in app components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Header } from "./components/core/Header";
+import { CustomQueryClientProvider, ToastProvider } from "./provider";
+import { RouteProvider } from "./provider/RouteProvider";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="fa-num bg-white dark:bg-black text-neutral-dark dark:text-white px-2 min-h-screen">
+      <div className="max-w-3xl mx-auto">
+        <CustomQueryClientProvider>
+          <Header />
+          <RouteProvider />
+        </CustomQueryClientProvider>
+        <ToastProvider />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
